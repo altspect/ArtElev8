95add2ffa8cc0544e10050ace920f593a56ff081
Jan Dyja <jandyja@Jans-MacBook-Pro.local>
2024-05-22 22:34:11 +0200

    Adding landing page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,12 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-  // { path: '**', redirectTo: '/sign-in' }, // Fallback route
+  { path: '**', redirectTo: 'landing' }, // Fallback route
+  {
+    path: 'landing',
+    loadComponent: () => import('./pages/landing/landing.component')
+      .then(m => m.LandingComponent)
+  },
   {
     path: 'sign-in',
     loadComponent: () => import('./pages/sign-in/sign-in.component')
